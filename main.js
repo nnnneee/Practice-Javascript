cb4623ef086be36f16fa316f7eae0cb4e7e86416
nnnneee <96486974+nnnneee@users.noreply.github.com>
2022-02-02 23:21:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let x = "Nadiya"
+document.getElementById("demo1").innerHTML = 'My name is' + x;
+let y = "CS-2112"
+document.getElementById("demo2").innerHTML = 'My group is' + y;
+let a = " 2020 ", b = " Wednesday ", c = " 02 ", d = "February", e = "10:38";
+document.getElementById("demo3").innerHTML = ' Year: ' + a + '<br>' + ' Today is : ' + b + '<br>' + ' Date: '+ c + '<br>' + ' Month: '+ d + '<br>' + ' Current time is: ' + e ;
+let z,h;
+z = 12;
+h = 10;
+w= z + h;
+q= z - h;
+document.getElementById("demo4").innerHTML = w;
+document.getElementById("demo5").innerHTML = q;
